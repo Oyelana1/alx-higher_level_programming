1fa649d871b4417ca8ec1afc00215bd6728e637b
Oyelana1 <140073282+Oyelana1@users.noreply.github.com>
2024-03-12 09:17:36 +0100

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const xzy = 'C is fun';
-console.log(xzy);
-
 const dab = 'Python is cool';
-console.log(dab);
-
 const law = 'Javascript is amazing';
+
+console.log(xzy);
+console.log(dab);
 console.log(law);
