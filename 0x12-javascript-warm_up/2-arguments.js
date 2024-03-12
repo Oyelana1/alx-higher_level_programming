afaa68317aadd078756d6efc788afca9a730375b
Oyelana1 <140073282+Oyelana1@users.noreply.github.com>
2024-03-12 09:56:14 +0100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,7 +4,7 @@ const x = process.argv.length - 2;
 if (x === 0) {
   console.log('No argument');
 } else if (x === 1) {
-  console.log('Arguments found');
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
